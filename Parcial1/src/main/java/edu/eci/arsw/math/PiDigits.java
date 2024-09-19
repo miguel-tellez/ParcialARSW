af81d52ca21d541f77381156bdc850f90ca04a06
miguel-tellez <77862071+miguel-tellez@users.noreply.github.com>
2024-09-19 09:54:11 -0500

    Update PiDigits.java

--- Parcial1/src/main/java/edu/eci/arsw/math/PiDigits.java
+++ Parcial1/src/main/java/edu/eci/arsw/math/PiDigits.java
@@ -6,108 +6,60 @@ package edu.eci.arsw.math;
 ///  https://en.wikipedia.org/wiki/Bailey%E2%80%93Borwein%E2%80%93Plouffe_formula
 ///  *** Translated from C# code: https://github.com/mmoroney/DigitsOfPi ***
 ///  </summary>
+import java.util.LinkedList;
 public class PiDigits {
 
     private static int DigitsPerSum = 8;
-    private static double Epsilon = 1e-17;
+    private static LinkedList<ThredpiDigits> threads = new LinkedList<>();
 
     
     /**
      * Returns a range of hexadecimal digits of pi.
      * @param start The starting location of the range.
      * @param count The number of digits to return
+     * @param N Los X numeros de hilos usados.
      * @return An array containing the hexadecimal digits.
      */
-    public static byte[] getDigits(int start, int count) {
-        if (start < 0) {
-            throw new RuntimeException("Invalid Interval");
-        }
+    public static byte[] getDigits(int start, int count, int N) {
 
-        if (count < 0) {
-            throw new RuntimeException("Invalid Interval");
-        }
+        Object lockObject = new Object();
+        LinkedList<Integer> digits = new LinkedList<>();
 
-        byte[] digits = new byte[count];
-        double sum = 0;
+        createThreads(start, count, N, lockObject);
 
-        for (int i = 0; i < count; i++) {
-            if (i % DigitsPerSum == 0) {
-                sum = 4 * sum(1, start)
-                        - 2 * sum(4, start)
-                        - sum(5, start)
-                        - sum(6, start);
-
-                start += DigitsPerSum;
+        for (ThredpiDigits Thread : threads) {
+            try {
+                Thread.join();
+                digits.addAll(Thread.getDigits());
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
             }
 
-            sum = 16 * (sum - Math.floor(sum));
-            digits[i] = (byte) sum;
         }
-
-        return digits;
+        byte[] digitsArray = linkedlistthreadArraylist (count, digits);
+        return digitsArray;
     }
 
-    /// <summary>
-    /// Returns the sum of 16^(n - k)/(8 * k + m) from 0 to k.
-    /// </summary>
-    /// <param name="m"></param>
-    /// <param name="n"></param>
-    /// <returns></returns>
-    private static double sum(int m, int n) {
-        double sum = 0;
-        int d = m;
-        int power = n;
 
-        while (true) {
-            double term;
-
-            if (power > 0) {
-                term = (double) hexExponentModulo(power, d) / d;
-            } else {
-                term = Math.pow(16, power) / d;
-                if (term < Epsilon) {
-                    break;
-                }
-            }
-
-            sum += term;
-            power--;
-            d += 8;
+    private static void createThreads(int start, int count, int N, Object lockObject) {
+        int digitsPerThread = count / N;
+        for (int i = 0; i < N; i++) {
+            int startDigit = i * DIGITSPERSUM;
+            int startInterval = i * digitsPerThread;
+            int endInterval = (i == N-1) ? count : start + digitsPerThread;
+            ThredpiDigits threadI = new ThredpiDigits(startDigit, startInterval, endInterval, i, lockObject);
+            threads.add(threadI);
+            threadI.start();
         }
-
-        return sum;
     }
 
-    /// <summary>
-    /// Return 16^p mod m.
-    /// </summary>
-    /// <param name="p"></param>
-    /// <param name="m"></param>
-    /// <returns></returns>
-    private static int hexExponentModulo(int p, int m) {
-        int power = 1;
-        while (power * 2 <= p) {
-            power *= 2;
+    public static byte[] linkedlistthreadArraylist(int count, LinkedList<Byte> digits) {
+        byte[] bytes = new byte[count];
+        int i = 0;
+        for (Byte b : digits) {
+            bytes[i++] = b;
         }
-
-        int result = 1;
-
-        while (power > 0) {
-            if (p >= power) {
-                result *= 16;
-                result %= m;
-                p -= power;
-            }
-
-            power /= 2;
-
-            if (power > 0) {
-                result *= result;
-                result %= m;
-            }
-        }
-
-        return result;
+        return bytes;
     }
 
 }
